55c95263eb7b45106203616029424a7d2b4323a0
linuxic <elennagornova@gmail.com>
2022-01-19 19:49:18 +0300

    Increment7

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -13,28 +13,23 @@ import (
 )
 
 var (
-	listen          *string
-	addr            *string
-	fileStoragePath *string
+	listen          string
+	addr            string
+	fileStoragePath string
 )
 
 func init() {
-	if listenFromEnv := os.Getenv("SERVER_ADDRESS"); listenFromEnv != "" {
-		listen = &listenFromEnv
-	} else {
-		listen = flag.String("a", ":8080", "Server address")
+
+	if listen = os.Getenv("SERVER_ADDRESS"); listen == "" {
+		flag.StringVar(&listen, "a", ":8080", "Server address")
 	}
 
-	if addrFromEnv := os.Getenv("BASE_URL"); addrFromEnv != "" {
-		addr = &addrFromEnv
-	} else {
-		addr = flag.String("b", "http://localhost:8080/", "Server base URL")
+	if addr = os.Getenv("BASE_URL"); addr == "" {
+		flag.StringVar(&addr, "b", "http://localhost:8080/", "Server base URL")
 	}
 
-	if fileStoragePathFromEnv := os.Getenv("FILE_STORAGE_PATH"); fileStoragePathFromEnv != "" {
-		fileStoragePath = &fileStoragePathFromEnv
-	} else {
-		fileStoragePath = flag.String("f", "links.log", "File storage path")
+	if fileStoragePath = os.Getenv("FILE_STORAGE_PATH"); fileStoragePath == "" {
+		flag.StringVar(&fileStoragePath, "f", "links.log", "File storage path")
 	}
 
 }
@@ -44,19 +39,19 @@ func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Kill, os.Interrupt)
 	defer cancel()
 
-	service := shortener.New(*addr)
-	err := service.Restore(*fileStoragePath)
+	service := shortener.New(addr)
+	err := service.Restore(fileStoragePath)
 	if err != nil && !errors.Is(err, os.ErrNotExist) {
 		log.Printf("Can't restore data from file: %s", err.Error())
 	}
 	log.Println("Starting server at port 8080")
 	srv := http.Server{
-		Addr:    *listen,
+		Addr:    listen,
 		Handler: NewRouter(service),
 	}
 	go func() {
 		<-ctx.Done()
-		err := service.Save(*fileStoragePath)
+		err := service.Save(fileStoragePath)
 		if err != nil {
 			log.Printf("Can't save data in file")
 		}
