73a40b52f52610898b6ebaf5fba4d1c7c40f35e6
linuxic <elennagornova@gmail.com>
2022-01-30 22:36:00 +0300

    Increment8

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -66,31 +66,19 @@ func main() {
 func NewRouter(service *shortener.Shortener) chi.Router {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Use(MiddlewareFunc)
+	r.Use(commonMiddleware)
 
 	r.Post("/api/shorten", makeShortenLink(service))
 	r.Post("/", makeShortLink(service))
 	r.Get("/{shortLink}", getLinkByID(service))
 	return r
 }
-
-type gzipWriter struct {
-	http.ResponseWriter
-	Writer io.Writer
-}
-
-func (w gzipWriter) Write(b []byte) (int, error) {
-	// w.Writer будет отвечать за gzip-сжатие, поэтому пишем в него
-	return w.Writer.Write(b)
-}
-
-func MiddlewareFunc(h http.Handler) http.Handler {
+func commonMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !strings.Contains(r.Header.Get("Content-Type"), "gzip") {
-			h.ServeHTTP(w, r)
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			next.ServeHTTP(w, r)
 			return
 		}
-		// создаём gzip.Writer поверх текущего w
 		gz, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
 		if err != nil {
 			io.WriteString(w, err.Error())
@@ -99,31 +87,15 @@ func MiddlewareFunc(h http.Handler) http.Handler {
 		defer gz.Close()
 
 		w.Header().Set("Content-Encoding", "gzip")
-		// передаём обработчику страницы переменную типа gzipWriter для вывода данных
-		h.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gz}, r)
+		next.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gz}, r)
 	})
 }
 
-func LengthHandle(w http.ResponseWriter, r *http.Request) {
-	// переменная reader будет равна r.Body или *gzip.Reader
-	var reader io.Reader
+type gzipWriter struct {
+	http.ResponseWriter
+	Writer io.Writer
+}
 
-	if r.Header.Get(`Content-Encoding`) == `gzip` {
-		gz, err := gzip.NewReader(r.Body)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		reader = gz
-		defer gz.Close()
-	} else {
-		reader = r.Body
-	}
-
-	body, err := io.ReadAll(reader)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	fmt.Fprintf(w, "Length: %d", len(body))
+func (w gzipWriter) Write(b []byte) (int, error) {
+	return w.Writer.Write(b)
 }
