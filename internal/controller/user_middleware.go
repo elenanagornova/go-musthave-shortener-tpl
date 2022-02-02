226d44069f5da40b32066de4225930d499eb7cbf
linuxic <elennagornova@gmail.com>
2022-02-02 22:25:44 +0300

    Increment9

--- internal/controller/user_middleware.go
+++ internal/controller/user_middleware.go
@@ -14,7 +14,6 @@ func UserMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		userUID := hellpers.GetUID(r.Cookies())
 		next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), UserCtxKey, userUID)))
-		return
 	})
 }
 
